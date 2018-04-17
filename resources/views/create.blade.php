9dc5b9bbcbe8fde09f5ba744d278fa00c73cfa92
Caio <caioparazzi@gmail.com>
2018-04-17 20:52:26 -0300

    setting up mongo and first post form

---
+++ resources/views/create.blade.php
@@ -0,0 +1,49 @@
+<!-- create.blade.php -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FiguredBlog</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">  
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>  
+  </head>
+  <body>
+    <div class="container">
+      <h2>Add new post</h2><br/>
+      <form method="post" action="{{url('posts')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="Name">Title:</label>
+            <input type="text" class="form-control" name="title">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="Name">Content:</label>
+            <textarea name="body" cols="50" rows="20"></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4" style="margin-top:60px">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <script type="text/javascript">  
+        $('#datepicker').datepicker({ 
+            autoclose: true,   
+            format: 'dd-mm-yyyy'  
+         });  
+    </script>
+  </body>
+</html>
